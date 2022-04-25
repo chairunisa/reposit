4be46162c509b8c3bc7b420b33a0195f58a25e24
chairunisa <67473431+chairunisa@users.noreply.github.com>
2022-04-26 00:39:47 +0700

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -4,12 +4,14 @@ import streamlit as st
 
 st.header("ANALISIS SENTIMEN REVIEW APLIKASI PEDULI LINDUNGI")
 
-df = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
      # To read file as bytes:
      bytes_data = uploaded_file.getvalue()
      st.write(bytes_data)
-df 
+     
+dataframe = pd.read_csv(uploaded_file)
+     st.write(dataframe)
 
 with st.sidebar:
     add_button = st.button(
