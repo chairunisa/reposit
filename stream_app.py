9d6595c39bf6860ae8087c302d41e304387297df
chairunisa <67473431+chairunisa@users.noreply.github.com>
2022-04-26 00:28:59 +0700

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -4,6 +4,12 @@ import streamlit as st
 
 st.header("ANALISIS SENTIMEN REVIEW APLIKASI PEDULI LINDUNGI")
 
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+     bytes_data = uploaded_file.read()
+     st.write("filename:", uploaded_file.name)
+     st.write(bytes_data)
+
 with st.sidebar:
     add_button = st.button(
        ("HOME")
