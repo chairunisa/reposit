21b8689305e50cddda3e98d1e5779a831a183048
chairunisa <67473431+chairunisa@users.noreply.github.com>
2022-04-26 00:46:27 +0700

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -5,9 +5,6 @@ import pandas as pd
 st.header("ANALISIS SENTIMEN REVIEW APLIKASI PEDULI LINDUNGI")
 
 uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-     bytes_data = uploaded_file.getvalue()
-     st.write(bytes_data)
      
 dataframe = pd.read_csv(uploaded_file)
 st.write(dataframe)
