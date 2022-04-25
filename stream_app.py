d70759feefe7fdbe822b94abe1c2d013a85e3286
chairunisa <67473431+chairunisa@users.noreply.github.com>
2022-04-26 00:22:16 +0700

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -3,6 +3,5 @@ import streamlit as st
 
 st.header("ANALISIS SENTIMEN REVIEW APLIKASI PEDULI LINDUNGI")
 
-st.sidebar("ANALISIS SENTIMEN"):
-left_column.button('HOME')
+st.sidebar("ANALISIS SENTIMEN")
 
