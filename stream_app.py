7c65545330b66d87c694fa3023fa7452f147adfc
chairunisa <67473431+chairunisa@users.noreply.github.com>
2022-04-26 00:36:01 +0700

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -9,6 +9,7 @@ if uploaded_file is not None:
      # To read file as bytes:
      bytes_data = uploaded_file.getvalue()
      st.write(bytes_data)
+df 
 
 with st.sidebar:
     add_button = st.button(
