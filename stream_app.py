68f6598118adeace062a3f2222b5274ad2561397
chairunisa <67473431+chairunisa@users.noreply.github.com>
2022-05-11 23:56:47 +0700

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -7,16 +7,7 @@ def navBar():
         {'id':"Sentimen Analysis", 'icon': "fab fa-chart-bar", 'label': "Sentiment Anlysisis"},
         {'id':"Visualization", 'icon': "fab fa-chart-bar", 'label': "Visualization"},
     ]
-    over_theme = {'txc_inactive': '#FFFFFF'}
-    menu_id=hc.nav_bar(menu_definition=menu_data, override_theme=over_theme, home_name='Home', first_selected=0)
-    return menu_id
-        
-        
-st.set_page_config(page_title='Analysis', layout = "wide")
-menu_id = navbar == 'Home':
-    st.write('---------------------------')
-    st.write('')
-    
+
     
 
 
