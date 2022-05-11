1d6aba9b1030a8dc83b27b747587cdaf6ecd9688
chairunisa <67473431+chairunisa@users.noreply.github.com>
2022-05-11 23:08:07 +0700

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,22 +1,11 @@
-!pip install streamlit-option-menu
+
 import streamlit as st
 import pandas as pd
-from streamlit_option_menu import option_menu
+
 
 st.header("ANALISIS SENTIMEN REVIEW APLIKASI PEDULI LINDUNGI")
 
-with st.sidebar:
-    selected = option_menu(
-        menu_title = "ANALISIS SENTIMEN",
-        options = ["Dashboard","Preprocessing","Klasifikasi","Visualisasi"],
-    )
-    
-if selected == "Dasboard":
-    st.title(f"UPLOAD DATA {selected}")
 
-    
-if selected == "Preprocessing":
-    st.title(f"YOU SELECTED {selected}")
 
     
 
