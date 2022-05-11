e1a1746d94eae24ab4c7b5cf9176201c4f5920d4
chairunisa <67473431+chairunisa@users.noreply.github.com>
2022-05-11 23:53:47 +0700

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -2,8 +2,22 @@
 import streamlit as st
 import pandas as pd
 
-
-st.header("ANALISIS SENTIMEN REVIEW APLIKASI PEDULI LINDUNGI")
+def navBar():
+    menu_data = [
+        {'id':"Sentimen Analysis", 'icon': "fab fa-chart-bar", 'label': "Sentiment Anlysisis"},
+        {'id':"Visualization", 'icon': "fab fa-chart-bar", 'label': "Visualization"},
+    ]
+    over_theme = {'txc_inactive': #FFFFFF'}
+    menu_id = hc.nav_bar(menu_definition=menu_data, ovveride_theme=over_theme, home_name='Home', first_selected=0)
+    return menu_id
+        
+        
+st.set_page_config(page_title='Analysis', layout = "wide")
+menu_id = navbar == 'Home':
+    st.write('---------------------------')
+    st.write('')
+    
+    
 
 
 
